11922e4fffe5c397b5044401f29edbab82bcbc7b
Kushal016 <67750474+Kushal016@users.noreply.github.com>
2021-04-14 14:49:28 +0530

    updated the connection call with remoteMysql

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ ob_start();//turns on output buffering
 session_start();
 
 $timezone = date_default_timezone_set("Asia/Kolkata");
-$con = mysqli_connect("localhost","root","","social");
+$con = mysqli_connect('remotemysql.com','rYchKMd5zM','QxQdOliAuk','rYchKMd5zM');
 if(mysqli_connect_errno()){
 	echo "Failed".mysqli_connect_errno(); 
 }
